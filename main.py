49d09b3fbe7a54596fa9b4c937d56a9c9302d2db
yefimets163 <107406655+yefimets163@users.noreply.github.com>
2022-12-13 18:16:26 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+from multiprocessing import Process
+import datetime
+
+result = 0
+
+def countTicket (start, end):
+    count = 0
+    for i in range(start, end):
+        num = str(i).rjust(6, '0')
+        if int(num[0]) + int(num[1]) + int(num[2]) == int(num[3]) + int(num[4]) + int(num[5]):
+            count += 1
+    print(f' from {start} to {end}: {count}')
+
+if __name__ == '__main__':
+    timeStart = datetime.datetime.now()
+
+    p1 = Process(target = countTicket, args = [0, 250000])
+    p2 = Process(target = countTicket, args = [250000, 500000])
+    p3 = Process(target = countTicket, args = [500000, 750000])
+    p4 = Process(target = countTicket, args = [750000, 1000000])
+
+    p1.start()
+    p2.start()
+    p3.start()
+    p4.start()
+
+    p1.join()
+    p2.join()
+    p3.join()
+    p4.join()
+
+
+
