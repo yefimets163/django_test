832ecb1e84264be436fe01c8709d7bba77c459be
yefimets163 <107406655+yefimets163@users.noreply.github.com>
2023-05-07 20:50:27 +0300

    Update views.py

--- shopinglist/shopinglist/polls/views.py
+++ shopinglist/shopinglist/polls/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from polls.models import ShoppingList, UserList, MallList, Item
+from .models import ShoppingList, UserList, MallList, Item
 
 def index(request):
     user_list = UserList.objects.filter(user_id=1).first()
-    result = ShoppingList.objects.filter(list_id=user_list.list_id)
 
-    new_result = [itm.__dict__ for itm in result]
-    return HttpResponse(str(new_result))
+    if request.method == 'POST':
+        item_name = request.POST.get('item')
+        amount = request.POST.get('amount')
+        shop_id = request.POST.get('shop')
+        shop_objct = MallList.objects.filter(pk=int(shop_id)).first()
+        item_ojct = Item(name=item_name, shop_id=shop_objct)
+        item_ojct.save()
+
+        new_item = ShoppingList(list_id=user_list.list_id, item_id=item_ojct, quantity=amount)
+        new_item.save()
+
+    result = list(ShoppingList.objects.filter(list_id=user_list.list_id).all())
+
+    return render(request, 'item_form.html', {'shoping_list_data': result, "shops": MallList.objects.all().filter(list_id=user_list.list_id)})
 
 def add_item(request):
     return HttpResponse("Add Item")
